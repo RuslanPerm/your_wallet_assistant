30c5aa9fab4156ffe7c2b0a05333285af7fdb9d8
Ruslan Zainetdinov <manfuckschool@mail.ru>
2022-11-25 23:56:25 +0300

    Бот: - устранены мелкие недочёты - исправлены все стоящие задачи: - теперь траты вводятся в виде: "-<стоимость>" - теперь последовательный диалог и при трате - создана защита при использовании если пользователь не зарегистрирован - оптимизирована программа

--- ywa_bot.py
+++ ywa_bot.py
@@ -5,12 +5,25 @@ import datetime
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 
+# функция обеспечивает последовательную отправку сообщений (возможно костыль)
+def take_mes():
+    vk_session = vk_api.VkApi(
+        token='360c9391be9e818c234c23236b6d52a8fc95949d3fb7113bae23c1b8392fcbd0bcff35aa316fd95c90e5e')
+    longpoll = VkBotLongPoll(vk_session, '212752190')
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            global vk
+            vk = vk_session.get_api()
+            return event.obj.message['text']
+
+
+# обработчик ошибок с базой данных
 def error_with_smt(event, error):
     print("Ошибка при работе с БД", error)
     answer(event, "Ошибка сервера, приносим свои извинения, устроняем проблему")
 
 
-def answer(event, message):
+def answer(event, message):  # функция, посылающая сообщения пользователя
     vk.messages.send(user_id=event.obj['message']['from_id'],
                      message=message,
                      random_id=random.randint(0, 2 ** 64))
@@ -20,18 +33,25 @@ def does_user_exist(ev):
     try:  # так как программа устанавливает в userid только целочисленные значения,
         # то пробуем узнать айди пользователя, если выдаст ошибку, значит пользователя не существует
         user_id = int(ev.object.message['from_id'])
-        return user_id
+
+        conn = sqlite3.connect('database.db')  # подключение к бд
+        cur = conn.cursor()  # создаём объект соединения с бд, к-й позволяет делать запросы бд
+        cur.execute(f"SELECT * FROM users WHERE userid = {user_id}")
+        if cur.fetchall():
+            return user_id
+        else:
+            return False
 
     except TypeError:
-        return 0.2
+        return False
 
 
+# функция, отвечающая сколько сбережений/накоплений у пользователя
 def my_bank(event):
     user_id = does_user_exist(event)
 
-    if user_id == 0.2:
-        return 'Похоже Вы не зарегистрированы в системе, что бы зарегистрироваться напишите мне ' \
-               '"как зарегестрироваться?"'
+    if not user_id:
+        return 'Похоже Вы не зарегистрированы в системе, чтобы зарегистрироваться введите: "?"'
     else:
         try:
             conn = sqlite3.connect('database.db')  # подключение к бд
@@ -51,9 +71,9 @@ def my_bank(event):
 def how_much_is_spent(event):
     user_id = does_user_exist(event)
 
-    if user_id == 1:
-        answer(event, 'Похоже Вы не зарегистрированы в системе, что бы зарегистрироваться напишите мне '
-                      '"как зарегестрироваться?"')
+    if not user_id:
+        return 'Похоже Вы не зарегистрированы в системе, чтобы зарегистрироваться введите: "?"'
+
     else:
         try:
             date = 1
@@ -102,9 +122,9 @@ def how_much_is_spent(event):
 def how_much_may_cost(event):
     user_id = does_user_exist(event)
 
-    if user_id == 1:
-        return 'Похоже Вы не зарегистрированы в системе, что бы зарегистрироваться напишите мне ' \
-               '"как зарегестрироваться?"'
+    if not user_id:
+        return 'Похоже Вы не зарегистрированы в системе, чтобы зарегистрироваться введите: "?"'
+
     else:
         try:
             conn = sqlite3.connect('database.db')  # подключение к бд
@@ -144,26 +164,43 @@ def how_much_may_cost(event):
 def expenses(event):
     user_id = does_user_exist(event)
 
-    if user_id == 1:
-        answer(event, 'Похоже Вы не зарегистрированы в системе, что бы зарегистрироваться напишите мне '
-                      '"как зарегестрироваться?"')
+    if not user_id:
+        return 'Похоже Вы не зарегистрированы в системе, чтобы зарегистрироваться введите: "?"'
+
     else:
-
         try:
-            text = event.obj.message['text'].split()
+            try:
+                cost = float(event.obj.message['text'])
+            except TypeError:
+                answer(event, 'Не понимаю, введите количество потраченных денежных единиц, введите ещё раз, пожалуйста')
 
+            exp_data = [user_id]
             conn = sqlite3.connect('database.db')  # подключение к бд
             cur = conn.cursor()  # создаём объект соединения с бд, к-й позволяет делать запросы бд
 
-            # exp_data = [user_id, sub_category_id(text[3]), text[3], text[2], text[1],
-            #             str(datetime.datetime.now())[:-10:]]
+            answer(event, 'К какой категории хотите отнести эту трату?')
+            exp_data.append(str(take_mes()))
 
-            exp_data = [user_id, text[3], text[2], text[1],
-                        str(datetime.datetime.now())[:-16:], str(datetime.datetime.now())[10:-10:]]
+            answer(event, 'Какая важность у этой траты: 1 - необходимое, 2 - развлечение')
+            try:
+                exp_data.append(int(take_mes()))
+            except TypeError:
+                answer(event, "Не понимаю, введите 1 если трата необходимая иначе 2 (пример: 1), попробуем снова")
+                expenses(event)
+
+            # answer(event, "Сколько Вы потратили?")
+            # try:
+            #     exp_data.append(float(take_mes()))
+            # except TypeError:
+            #     answer(event, "Не понимаю, напишите количество денежных единиц Вы потратили (пример: 999.9)")
+            #     expenses(event)
+            exp_data.append(cost)
+            exp_data.append(str(datetime.datetime.now())[:-16:])  # записывает дату траты
+            exp_data.append(str(datetime.datetime.now())[10:-10:])  # записывает время траты
 
             cur.execute("INSERT INTO expenses (userid, category_name, importance, costs, date, time) "
                         "VALUES(?, ?, ?, ?, ?, ?)", exp_data)
-
+            # сделать чтобы айди траты заносилось или генирировалось
             # сделать чтобы вычитал из бюджета
 
             conn.commit()
@@ -175,96 +212,76 @@ def expenses(event):
 
 
 # зацикливается если у последнего элз убрать условие, да и в таком виде багает
-
-# функция обеспечивает последовательную отправку сообщений (возможно костыль)
-def take_mes():
-    vk_session = vk_api.VkApi(
-        token='360c9391be9e818c234c23236b6d52a8fc95949d3fb7113bae23c1b8392fcbd0bcff35aa316fd95c90e5e')
-    longpoll = VkBotLongPoll(vk_session, '212752190')
-    for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            global vk
-            vk = vk_session.get_api()
-            return event.obj.message['text']
-
-
-def registration(event):
-    answer(event, 'Как мне к Вам обращаться?')
-    user_id = event.object.message['from_id']  # id отправителя присваивается id пользователя в системе
-    name = take_mes()
-
-    answer(event, 'Выберите Ваш финансовый план и напишите мне его номер\n\n'
-                  '1 - экономный: от Вашего бюджета 40% на необходимые вещи, 20% на другое 40% на сбережения\n\n'
-                  '2 - стандартный: от Вашего бюджета 50% на необходимые вещи, 20% на другое 30% на сбережения\n\n'
-                  '3 - "жизнь одним днём": от Вашего бюджета 50% на необходимые вещи, 20% на другое '
-                  '30% на сбережения\n\n'
-                  'К необходимым вещам относятся: оплата жилья, продуктов, ЖКХ, связь, образование, налоги и т.д.\n\n'
-                  'К другому относятся: развлечение (кафе, доставка еды, аксессуары и т.д.)\n\n'
-                  'Заостряю внимание, что я лишь подсказываю, реальное распределение трат '
-                  'по важности исключительно Ваш выбор')
-    f_plan = take_mes()
-    answer(event, 'Установите пароль (более 8 символов) для входа в систему с приложения')
-    password = take_mes()
-    answer(event, 'Какой у вас бюджет на этот месяц?')
-    budget = take_mes()
-    answer(event, 'Скажите, у Вас есть какие либо сбережения уже (именно сбережения, а не бюджет), если нет, введите 0')
-    acc = take_mes()
-    full_data = [user_id, name, budget, f_plan, str(password), acc]
-
+def delete_info(event):
     try:
         conn = sqlite3.connect('database.db')  # подключение к бд
         cur = conn.cursor()  # создаём объект соединения с бд, к-й позволяет делать запросы бд
 
-        try:
-            cur.execute("INSERT INTO users (userid, name, budget, f_plan, password, accumulation) "
-                        "VALUES(?, ?, ?, ?, ?, ?)", full_data)
-        except sqlite3.IntegrityError:
-            answer(event, 'Похоже с вашего аккаунта ВК уже был выполнен вход в YourWalletAssistant, '
-                          'желаете стереть данные о нём или продолжить работу в этом пользователе?\n\n'
-                          'Отправьте мне "стереть" если желаете стереть данные о нём\n'
-                          'Отправьте мне "ладно" если желаете продолжить')
-            if take_mes() == 'стереть':
-                # удаляем строку с данными пользователя с данным id
-                cur.execute(f"DELETE FROM users WHERE userid = {event.obj.message['from_id']}")
-                answer(event, 'Данные успешно удалены')
-                conn.commit()
-                cur.close()
-                return 0
-            if take_mes() == 'ладно':
-                print('тут норм')
-                cur.execute(f'SELECT name FROM users WHERE userid = "{user_id}"')
-                print('тут тоже')
-                name = cur.fetchone()[0]
-                answer(event, f"Хорошо, тогда рад новой встрече, {name}")
-                conn.commit()
-                cur.close()
-                return 0
-        finally:
+        deciding_of_user = take_mes()
+        if deciding_of_user == 'стереть':
+            # удаляем строку с данными пользователя с данным id
+            cur.execute(f"DELETE FROM users WHERE userid = {event.obj.message['from_id']}")
+            cur.execute(f"DELETE FROM expenses WHERE userid = {event.obj.message['from_id']}")
+            answer(event, 'Данные о пользователе и его тратах успешно удалены')
             conn.commit()
             cur.close()
+
+        elif deciding_of_user == 'ладно':
+            cur.execute(f"SELECT name FROM users WHERE userid = {event.obj.message['from_id']}")
+            name = cur.fetchone()[0]
+            answer(event, f"Хорошо, тогда рад новой встрече, {name}")
+            conn.commit()
+            cur.close()
+
     except sqlite3.Error as error:
         error_with_smt(event, error)
-    answer(event, f'Приятно познакомиться, {name}, Вы успешно зарегистрированы!')
 
-    # try:
-    #     u_d = event.obj.message['text'].split()  # user data данные о пользователе
-    #     user_id = event.object.message['from_id']  # id отправителя
-    #
-    #     conn = sqlite3.connect('database.db')  # подключение к бд
-    #     cur = conn.cursor()  # создаём объект соединения с бд, к-й позволяет делать запросы бд
-    #
-    #     full_data = [user_id, u_d[1], u_d[2], u_d[3], u_d[4], u_d[5]]
-    #     cur.execute("INSERT INTO users (userid, name, budget, f_plan, password, accumulation) "
-    #                 "VALUES(?, ?, ?, ?, ?, ?)", full_data)
-    #     conn.commit()
-    #     cur.close()
-    #
-    #     answer(event, f"Приятно познакомиться, {u_d[1]}, первый этап регистрации Вы прошли!\n\n "
-    #                   f"Небольшая рекомендация по использованию бота: периодически очищайте диалог с этим ботом для "
-    #                   f"профилактики утечки данных")
-    #
-    # except TypeError or sqlite3.Error:
-    #     answer(event, "Не могу понять Ваши данные, введите пожалуйста в виде доступных мне символов")
+
+def registration(event):
+    us_id = does_user_exist(event)
+    if us_id:
+        answer(event, 'Похоже с вашего аккаунта ВК уже был выполнен вход в YourWalletAssistant, '
+                      'желаете стереть данные о нём или продолжить работу в этом пользователе?\n\n'
+                      'Отправьте мне "стереть" если желаете стереть данные о нём\n'
+                      'Отправьте мне "ладно" если желаете продолжить')
+
+        delete_info(event)
+    else:
+        answer(event, 'Как мне к Вам обращаться?')
+        user_id = event.object.message['from_id']  # id отправителя присваивается id пользователя в системе
+        name = take_mes()
+
+        answer(event, 'Выберите Ваш финансовый план и напишите мне его номер\n\n'
+                      '1 - экономный: от Вашего бюджета 40% на необходимые вещи, 20% на другое 40% на сбережения\n\n'
+                      '2 - стандартный: от Вашего бюджета 50% на необходимые вещи, 20% на другое 30% на сбережения\n\n'
+                      '3 - "жизнь одним днём": от Вашего бюджета 50% на необходимые вещи, 20% на другое '
+                      '30% на сбережения\n\n'
+                      'К необходимым вещам относятся: оплата жилья, продуктов, ЖКХ, связь, налоги и т.д.\n\n'
+                      'К другому относятся: развлечение (кафе, доставка еды, аксессуары и т.д.)\n\n'
+                      'Заостряю внимание, что я лишь подсказываю, реальное распределение трат '
+                      'по важности исключительно Ваш выбор')
+
+        f_plan = take_mes()
+        answer(event, 'Установите пароль (более 8 символов) для входа в систему с приложения')
+        password = take_mes()
+        answer(event, 'Какой у вас бюджет на этот месяц?')
+        budget = take_mes()
+        answer(event, 'Скажите, у Вас есть какие либо сбережения уже (именно сбережения, а не бюджет),'
+                      ' если нет, введите 0')
+        acc = take_mes()
+        full_data = [user_id, name, budget, f_plan, str(password), acc]
+
+        try:
+            conn = sqlite3.connect('database.db')  # подключение к бд
+            cur = conn.cursor()  # создаём объект соединения с бд, к-й позволяет делать запросы бд
+
+            cur.execute("INSERT INTO users (userid, name, budget, f_plan, password, accumulation) "
+                        "VALUES(?, ?, ?, ?, ?, ?)", full_data)
+            conn.commit()
+            cur.close()
+        except sqlite3.Error as error:
+            error_with_smt(event, error)
+        answer(event, f'Приятно познакомиться, {name}, Вы успешно зарегистрированы!')
 
 
 def main():
@@ -284,11 +301,10 @@ def main():
                 mes = "Приветствую, я воспринимаю лишь следующие команды (Если я тебя не пойму, " \
                       "то соответственно не отвечу!):\n\n " \
                       "Если Вы зарегистрированы в системе 'Ваш финансовый помощник':\n\n" \
-                      "1) 'потрачено <стоимость> <уровень важности (1 [необходимое] или 2 [другое])> " \
-                      "<подкатегория (название траты)>'\n" \
-                      "2) 'остаток?'\n" \
+                      "1) '-<число> (пример: -999.99)'\n" \
+                      "2) 'остаток'\n" \
                       "3) 'за <дата>'\n" \
-                      "4) 'сбережения?'\n\n" \
+                      "4) 'сбережения'\n\n" \
                       "Если Вы не зарегистрированы:\n\n" \
                       "1) 'как зарегистрироваться?'\n" \
                       "2) 'зачем ты мне?\n" \
@@ -307,8 +323,7 @@ def main():
 
             elif event.obj.message['text'].lower().startswith('что ты умеешь'):
                 answer(event, 'Мои возможности:\nЧтобы добавить трату отправьте мне: '
-                              '"потрачено <стоимость> <категория> <подкатегория>"\n'
-                              'Пример: "потрачено 100 1 продукты"\n\n'
+                              '"-<число> (пример: -999.99)"\n\n'
                               'Чтобы узнать сколько Вы можете потратить отправьте мне:\n'
                               '"остаток"\n'
                               'Чтобы узнать сколько Вы потратили за определённый день отправьте мне:\n'
@@ -316,7 +331,7 @@ def main():
                               'Чтобы узнать сколько у Вас накоплений отправьте мне: '
                               '"сбережения"')
 
-            elif event.obj.message['text'].lower().startswith('потрачено'):
+            elif event.obj.message['text'].lower().startswith('-'):
                 expenses(event)
 
             elif event.obj.message['text'].lower().startswith('остаток'):
